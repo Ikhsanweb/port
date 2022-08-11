a78ace95805c85ed60af27dc561494d2f1143b06
Muhammad Ikhsan <ikhsan.dev147@gmail.com>
2022-08-11 15:30:22 +0700

    Version 3.0 included2

--- src/comp/Header/HeaderNav.jsx
+++ src/comp/Header/HeaderNav.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {FaDribbble} from 'react-icons/fa'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+// import {BsLinkedin} from 'react-icons/bs'
+// import {FaGithub} from 'react-icons/fa'
+// import {FaDribbble} from 'react-icons/fa'
 
-const HeaderSocials = () => {
-	return (
-		<div className="header__nav">
-			<Link to="/profile">Profile</Link>
-			<Link to="/portfolio">Portfolio</Link>
-			<Link to="/experience">Experience</Link>
-			<Link to="/contact">Contact</Link>
-		</div>
-	)
-}
+// const HeaderSocials = () => {
+// 	return (
+// 		<div className="header__nav">
+// 			<Link to="/profile">Profile</Link>
+// 			<Link to="/portfolio">Portfolio</Link>
+// 			<Link to="/experience">Experience</Link>
+// 			<Link to="/contact">Contact</Link>
+// 		</div>
+// 	)
+// }
 
 
-export default HeaderSocials
+// export default HeaderSocials
